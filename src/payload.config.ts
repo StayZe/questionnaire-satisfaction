059a74c8726160f2526aaca2a82c43e424230fbc
Comsea <mandra.antoine@gmail.com>
2026-03-31 23:34:34 +0200

    final 2

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,7 +6,6 @@ import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
 import { Users } from './collections/Users'
-import { Media } from './collections/Media'
 import { Reponses } from './collections/Reponses'
 
 const filename = fileURLToPath(import.meta.url)
@@ -19,7 +18,7 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media, Reponses],
+  collections: [Users, Reponses],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
